391f2d70b1f34450dd27c976cea81026976ca78f
eduwht <eduwht2@gmail.com>
2017-02-22 13:22:52 +0100

    Basedades

---
+++ Serilont-master/WebSite4/App_Code/ErroresSQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de ErroresSQL
+/// </summary>
+public static class ErroresSQL
+{
+    public static String MensajeSQL(SqlException sqlEx)
+    {
+        String mensaje = "";
+
+        switch (sqlEx.Number)
+        {
+            case 547:
+                mensaje = "No se puede borrar, tiene registros relacionados";
+                break;
+            case 2627:
+                mensaje = "El registro ya existe.";
+                //contexto.ciudades.Remove(ciudad);
+                break;
+            case 2601:
+                mensaje = "No se puede insertar este registro ya que estaria duplicado.";
+                break;
+            default:
+                mensaje = sqlEx.Number + " - " + sqlEx.Message;
+                break;
+        }
+        return mensaje;
+    }
+}
---
+++ Serilont-master/WebSite4/App_Code/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Model
+/// </summary>
+public partial class Usuaris
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string email { get; set; }
+    //public DateTime data_naixament { get; set; }
+    //public string sexe { get; set; }
+}
