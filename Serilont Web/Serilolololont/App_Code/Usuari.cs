24dcda77560f81e9c2668cfffc44eab6d5791bcb
Kuantize24 <asune24@gmail.com>
2017-03-02 11:07:15 +0100

    Add files via upload

---
+++ Serilont Web/Serilolololont/App_Code/Usuari.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Usuari : IdentityUser
+{
+    public String nomComplet { get; set; }
+     
+    //public String contrasenya { get; set; }
+    
+}
---
+++ Serilont Web/Serilolololont/Login.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Web.Security;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonEntrar_Click(object sender, EventArgs e)
+    {
+        if (ORM.comprovarUsuari(TextBoxNomUsuari.Text, TextBoxContrasenya.Text))
+        {
+            Session["Usuari"] = ORM.buscarUsuari(TextBoxNomUsuari.Text);
+            FormsAuthentication.RedirectFromLoginPage(TextBoxNomUsuari.Text, false);
+        }
+
+        else
+        {
+            MostrarMissatge(PanelMensaje, LabelMensaje, TipoMensaje.Error, "Usuari o contrasenya incorrecta.");
+        }
+    }
+
+    protected enum TipoMensaje
+    {
+        Error, Correcto
+    }
+
+    protected void MostrarMissatge(Panel p, Label l, TipoMensaje tm, String mensaje)
+    {
+        if (tm == TipoMensaje.Error)
+        {
+            p.CssClass = "alert alert-dismissible alert-danger";
+        }
+        else if (tm == TipoMensaje.Correcto)
+        {
+            p.CssClass = "alert alert-dismissible alert-success";
+        }
+        l.Text = mensaje;
+
+        p.Visible = true;
+    }
+
+    private void netejarDades()
+    {
+        TextBoxNomUsuari.Text = "";
+        TextBoxContrasenya.Text = "";
+    }
+}
